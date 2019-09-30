389ed20db127af747f1a4a10db7da0c5e8c9125f
AnonymousPomp <anonymouspomp@gmail.com>
2019-09-30 16:25:08 +0800

    .

--- source/ComicDownloader.py
+++ source/ComicDownloader.py
@@ -4,13 +4,14 @@
 more information cam be found at usage()
 
 how to make it an executable file:
-    pyinstaller --onefile --windowed --icon=./Images/icons8-pluto-dwarf-planet-48.png ComicDownloader.py --hidden-import=queue
+    pyinstaller --onefile --windowed --icon=icons8-pluto-dwarf-planet-48.png ComicDownloader.py --hidden-import=queue
 
 # todo: remove sys.exit()
 # todo: remove print (replace it with log())
 # todo: make log window sizable
 # todo: iterators -> generators
 # todo: remove unnecessary image loading
+# todo: add prevsearch
 """
 __author__ = "Anonymous Pomp <anonymouspomp@gmail.com>"
 
@@ -78,6 +79,12 @@ COMIC_TYPE_NAVER = 1
 COMIC_TYPE_NAVER_BEST_CHALLENGE = 2
 COMIC_TYPE_DAUM = 3
 COMIC_TYPE_KAKAO = 4
+
+IMG_DOWNLOAD = b"iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAA8FBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABmRfStAAAAT3RSTlMAAQQICQoLDBIXHCAhJCcoKiwtLi8xMjM0Njg7PD5DREVOT1ldYmNncHFzd3iChYaIj5SVl5ibnqCipbC1t8PHyM7R1drc4OTo6fX3+fv9YEV6XQAAAOVJREFUGBnVwddWwkAARdETldgbYyzYFbFXxK6IWBCE+/9/IziZSVjIu+7NXxIsXVTr1dNp+hh/k1Ue4Tc5eV8T9BpTSiOkR0Vp1wP5u6scKZPqVldbicSerN0wDAty1vBuZEWAkfOA9ygrAoycCrFguyUrAoycE6zgXk4EGDn5gB/H8iLAyKvN0TYq68AYMwQMGmP2FVsFDmXtkCgo1hyGF8U2cbbkHUFDzjrWhhI1eJW3TMeK0gLOlVgAFpXWhKxSZsmqyy3wpETrrKUuM0DmU30V6cg8q49igDVfelePj8sp/otvn/iK48fNmCQAAAAASUVORK5CYII="
+IMG_SEARCH = b"iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAABCFBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAzxr8JAAAAV3RSTlMAAQIDBAYHCw4PERMUGBseIyQmKCksLjEzNDU3ODo7PENGR05PUFRXW11kZnBxdXd/gIKDkZ2eoKWor7C0t7m8xcfIyszO0dPV19re4OTm6evt7/H19/ntlmEiAAABOElEQVQYGX3BCSMCURgF0DtTTSghITuh7OtESnYKjWSZ+///ie89NUs1zoFnbP34sl45Wk1iqJlH9tQyGDB6w6ByAmG5b4a10gia5QA3Dd/YD5XXwoiJWKrUodJKwPNEZcfAn9gplTJ6lqjMw7dNJYOuFkURQWcUNfyZoLhHSKxDkYS2SzGHsBLFKrQqRRxhKYojaA7JNvrEKCrQXJJ36EdRh9Ym6aCPSXEB7ZrCRNgIxQm0A4oswgoUa9ByFFWEGK8UKWjGF0UeQTsUj+jaonDH4Vugsowu06Fw8+gpUrmFZ5JaNWsCiM89UDuHb5Ndzt0HPTZ8mxzGhm/SYVDzhYoNn7HxyZ73DcNqUrERYEzv19vu29XelAHAalKxEclqULERyWpQWUEkq0FxgGjWM8kc/hE/rCziF8m9fWG4iWOGAAAAAElFTkSuQmCC"
+IMG_BROWSE = b"iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAb1BMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABt6r1GAAAAJHRSTlMAAgMEBQgJCyMkQElVVleFi4yRlZidnrm8wMHDxcjM3uDk5vvXt23NAAAAp0lEQVQ4y83SuxaCMBAE0CWJKFmUlzzloWb+/xstULEgW4iFU+65zcxZou0JCwdPXBESmRuEXDUdISamTAYp5TLIfwMq3wLVE5Q+UH4DjJGBGQYtAd0DvfYDfQGATvuAaufurVoHqnmt06g1ENTLfnWwAiJm5g7omJkPvpoJkIg7/CnIgNFaa62NFhDNlxFI6fTxXW+w/GFM5i6BSRPtz84HXL6j7XkAv4hAOfO2KdAAAAAASUVORK5CYII="
+IMG_LOGO_48 = b"iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABmJLR0QA/wD/AP+gvaeTAAAGeklEQVRoge2Zf0zU5x3HX8/dcXBwx2/E4QaiTvyRaifa1qoUrakahZEsumwmc1m2bGuUNdmPZFmykS1Ltv61P+aSbpN203S1TU0mLbW0a6mmjSJaOaIZ7QRBQSi/Oe64O+6+n/0hQ4Tv9+77PTDLMt7J/fF9Ps/n+bzfz+f5fbCIRSzifxpqoRqSN/avwG7fjZJdCEVA/tRPAUPAANCKkougGtXeuusLEXdeAuT98hSC6d9AyXMIay26f4zwIq6UF9XO18YT5ZCQAKmpsfHE1WNo8lMU+YkGn0IvqF/gSz6hDr0WtepsWYC8XVGM8BJCmVXfOGhCix5S++s7rTjZrFQ+dmBXRTAo1x4CeYDHsNmvSn3FPitOpgVU7y8/AnLm7pDmsc7NNLJR/F3qK6rMOpgSUF2x88ui1AnA0dFreZhaRRKK03Kucq+ZynEFHK0oXyPCKcAO0NIeXbClNwaciLwsb1Utj1cxpoCa8nKHEvUK4P5PWcfdKJ19Dz0LAFkQfVlePWiPVSmmgCG3qgY2zi5/pTFEaFLmyc8UtuIOHYlVwXA4/KRym2dCc94CsvXsuRk2tq518PlcG7mZNrLdCrv9oYyuHmyh1WpPg1/P6DDymtCc38OAPMDAqEbdxfD0t80GWW4beRmK5UvtbFzhYFmupVVaF6N+KTj5nvZjoEbPbigA+JaVQJoGg2Mag2Pwz9tRzl0Os67IzlfLk8lyJy7k9QshOnq0r2EgQDfnxw7s3AhcSzjqDHhSFdVVLvKz5ooIR+DVxiAt7VGWZisOPO6k5Av3+/TKpxH+0hAEQNNsJcfr//HJ7DZ0u0bB0wtBHsAXEP5QN8Go/8FJH47AC29O0NQWITQpdPZpHD8b5NS7QYbHNdpuR/jbe8H7RO2a7r6gP4QUT7CAi8ywT3jhjSDPfcWF03FvuNWem+DTO3OX46a2CE1tkbmNCI/pta2bARFK5sl5Du4MRPlrQ5CxgHDy3SA3Oq3tJQLr9cp1MzA45nflpKclQDM2vB0RvB06vWsCCgr0ynUzMD4RLB4dn0go0ENEhl6h/vomhEfGA/j8QV3zfwm6qdMdQgr6BYqGfH7sdkVqSrLpKEXLPGzfUsCqokzSPU7Ck1H6+gNc+riPJm8v0WiCq4PQb1qAKAamLub0j4yTl6VITXbGjbGvfDl7nypEqfvbS5LDxorCDFYUZrBt8+c4cfoGw2MJZFaJrgD9VQjaZn73D/sIBMN6Vaexe3sh+8qLHiA/G4XLPBz95gbcafE7Yy7UnE0MDDJgE3VekK/PLOsf8ZHhdpHpTp1TPzszhX3lRaZo5GW7OHRgFbWnb0yXZaYn8/ij+ZSszGJJTippriT8gTB3+vxcaOrh+ieDKOG8aQGaaB/o9eTo+ASTEY2cjFRs6n7ynixdSpLD/Hnn0bV5rFmZRVv7MM/sKGRPWRGOWf7pnmTWeZJZtyqb85e6OfP2vz7Qa8sw35WbN3iBR/RsDrud7Iw0XM4kAH70nU0ULrN2Ve7q8dHV7WP7Ft3l/QH4A5N33WW/Nr8PACjUcSNbJBrls6Ex+kfGCUei5Oe5TJGeicICjynyAGlpSc8b2QwFTITllNx7EjREIBiid3AEhyPmrW++GCCFPxsZDQU0eL1+Jepn8VoXgd7+hF8GzeBXan2NYYCYM2/TlZY/gvooXoSPrnYnQiw+hGba1xkOZZh6KjFCI0hJQf6HKA4DhgO9d8DPnh3FOOyJ37zev9jFz393gfrG9r7IpHZtVVGm3+5MOqh2PzsQy8/ULbxyyyNliGoADM8Ue3YU8+zhL1mkfQ83O4f54W8bEW36mBEWpKquufWteL6mZl9bz2edqwvybyqoMPK52TWCw65Y/8Vc08QBxgNhnv9TE0Mj08eLSeBgXXPrm2b8Lb2DVJZu3IlNXkfIMqpTfaSUp7fG3pVFhMutvZxvus3l1l6CoemD5oCIOlx3paXBLCfLDzmVmzeUgKoFeVLPnpPpovY3sR+Yf3/yKu98eGt28SWR6KG6K9e7rPCxPOvONnvbNjW37EDJd4HB2fa87LlnpdlobLo987NH4NvJxSXbrJKHef7FVFFamqps4cOIOrokJ23lM9uXj1bsWpmXkuxIiuVX9f0ziMhlEdvJ0KTUNni9uq9uZrBwf/I116xB1FMoKUNYjWIJkDsVYxgYRHHtVrev+we/fOels83ettgtLmIRi/i/wL8Bk/A+VLWiHtYAAAAASUVORK5CYII="
+IMG_LOGO_96 = b"iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAYAAADimHc4AAAABmJLR0QA/wD/AP+gvaeTAAANXElEQVR4nO2deXRU133HP/fNSBptI4RWkA4IYgsMAgESW5JiYScsAgHm2KdOGsfJaeqUeml82nOS09op7nbqpMexoeQ0du00dtM6jmuENsCcgu3Yji0JkNiMZJBBIIGE9m1mNPPe7R+yCMJa3jYjYc/nz5n7+/3u3O+79919IEyYMGHChAkTJkyYMGHChPkiISY7A+MhX7svGvdAPpqSB/JLCDEHmAPCDTIWiAccQBcQQNKCoBG4hOQ0ilaDz1Ejtpb0TubvGI8pJ4A8uCkPqdyDZD2QC0RYdBkAqkD8H0IrFRvKKq3n0j6mhADyUNEsVB5C8i1gdpDDNSD4Hxw8L75e2hjkWBMyqQLIii1fQcgfAoUMNSWhRAX2oSk/EZv2fRji2NeZFAHkgaJlSP6BoYKffCRlSPVJsamiJtShQyqA3LttGi71KeARQAllbB1owK+JcD4uvra3PVRBQyaArNh8L0LsBtJDFdMkV0H+qdhYVhGKYEEXQB4pcOGLfwbJjmDHshEJ4udI/1+Jwv2+YAYKajPwcOFds/198e/dYoUPIEA+jHC+JQ9snxHMQEET4LFNBTmKIt9t7dRygxUjBKxC+qtk+dZFwQoQFAEe23T3WqmId4HMpnZtSow1LJCBQ3tHHtz65WA4t12AhzffvVoKrQxJAkBzm3qrCwCSaWjaQVlRtMpu17YK8IPCgiWK0PYDMcOfXWrT7AwxmcQhOCDLC5fY6dQ2AR4pWjNHVcT+4Sd/mPNXVNHvlXaFmWwSUBxlsrzQtq60LQI8unFjlJCO3zJKH1/T4OQnqh1hpgoZKI43ZMXGKDuc2SKAULzPAHljfV9z3m9HmKnEanA8bYcjywI8urngXin4i/HSfNSo0vT5eRcMIcRjsqJoo1U3lgR4dONGN4jnJkonJeyvHLQSaioiEOJ5uW9LvBUn1mqAw/NPwEw9SU9eCHD20ufqXQDITCLlj614MN1Hf7jormWKlJUYmMePixY8ujWaGUlTbSLUEn5ULVdsLv/IjLHpklCQf4/BRZQ+j+SZ//VwoHqQ3oHPTdc0AofylFljUzXg06e/2qz9cOCMZAczkxWS3YKUaQrJboXkBEGs65YbPEuEXCo2lNUaNXSaiSak9gQIS6UkgcttKpfbPvteiI4SJMUPiZGcoJA+XWFuukJywtRsuqREvHrY93fAdqO2hgvxB9sKstSAaDBja5WEWMGy252smBdBRvLUEaOqLsCvD3v9ii8y/WcHD3YYsTX8KwJ+8QCTtJbc3S85UuPn6d8MsKfEy5WOyR9bDHih+D0fmkZEwOn/tlF7owIIIcQDRoMEg7pLAX7ymofDxyd3fLH3PR+9nqEOhRQ8ZNTe0JP8l4VrV2oKHxgNEmxWzHPyjbVROBzGK6ZnUFL+4SDV9X6kJpiZLFgxL4KV8yNQJng8T11QeaHCg7yhQ+eQ6oJny9/R3SU19BLWHKxnCvYeK+sC+FV4cJ0LxYAGTW0a/17mobt/+EdJzjdLzjf7eOekn/vujGJu+ug97WtdGq8c8o4ofICA4tgA6BbAUBMk4W4j6UPJ8XMBXj2if/38k6sau4pvLPyRNLVpPPeGh9fe9uHzj0zj80v+Y78Xz+BnbYVkg5F86x5I/fW6dbGqQ9ttxCbUXG7TiHDC3BnjZ/FKu8aeEg8e38TVubFV42h9AFekYFqcQmu3xgsVXprbx+wAZHwld8YzH5y5HNCTZ91NkMcZyFMgUm/6yaL094OkT1fIyRr9p3X2afy81MOAjsIfpqNX8t+HfYCuGuby90ctBKr1JNbdBCkO7Q69aScTCfzqTd+oXVSPT/KLMu+YzY5dCEUs0JtWtwBCE/PMZSf0+PySF/ePfMoHA/B8hWe8psNOcvQm1C2AJrhlBABo7ZLs2uulqU2jqU1j1xsezjeHaOAm0F0DdL8DOrr705ISYs1laJJobld5+jcDoQ+skaY3qe4a0O/xzWjv6TeXoS8aQiRMnGgI3QJIZGTfgJeuvkl4om45pP0CAG6A7j4Pvf0ew1n6ghEUAa6n7egdoM8T1F3btzbCQPdev1c5ou1p7+7D4/vc7XSwB4nul6WByTgxwKfN0DDXunpJTUzAFWlqYc0Q8bGR5C5IJivTTWZ6LHGxkUS7nICkb8BP89V+zl3spvpEC929k/tgSIIiAO3ctPVQSrjW2Uva9HgiI4IjQlpKLEV3ZZEzLwlljKnORLeDRLeLhdlJFH1tDlW1LZQdvkB3z+Q0k0II3atiRkqtEVh484ea1Gjt7CE10W2rCELA+jWz2XDn7DELfjQUIVi5JJ3F85N5tbSe46ev2ZYnvQjJRb1p9b8DBBfG+krVJK2dPfgD9my8UhTBd+9bQOHaLEOFfyPRLiffufcO1q+ZZUuejCCRQRAA6sb7UtUkLR3dtohwf1E2SxakWPYjhGDTXXMoWJ1h2ZehuFKMW1Y3on8gpomjE6UZFsHn1zUVPirLc9NYtdTek6z3rLuN3AXJtvocD6loVXrTGmi0ncfAH5jIRtUkLe09JE+LI8ZlbPnAFeVg27ovGbLRgxDwrW3zudx8lPauiQeRqUnRzJ2VwNxZCaQlxxAb4yTG5SQmJgKfT6Wr18elpl5O1LVzur4dVR0xvR1wuny6N2gZamC35C+qBLFcn2fB9PgY4mNcuv2vXZ3JPevtF2CY+oZO9rxy4jPruACRkQ7yF6fy1fwZZKbr3/Dc0eWl+M0Gas5cf9l/sLvsyGq99oaWF+fNTM8ACvSm9/j8+AMa0VFOhI6NdH+yNZu42OAtuiUlRtPW6aW5ZWQ3fenCFL7/zRzyF6XhjjN28CXa5WTpwhQSE1ycOdeBhBcq6y+8rdfe0KK8gjB8fH/A66O5rRvv4PinZLIy3aSlBH+6u7Ag6/r2lQing29vn89371vANLe1E0erlqbzwD3ziYqMOGjEzpAAxdW1VUCDoZwBqqrR0tFDR28/crT6D2TPnWbUrSmSEl3kLUojwulgxwM55C/WPXU/IXfcNr3tp6+/aehCKKM74yTwikGb6/T2e7na3jNqL2lOpnsUi+CwZsVM7i+6ndtm2yt6dFTEs0IY2zlleG+oKuTLDF3tYorBQICrHT20d/ehan9wk5oUbdalYWbNjGd5rn1P/qdoSMfLRo0MC1BedbIBKfcZtRuBlPR5fDRf66Kzd4CAquKOn/I7XsZHUixWPHHJqJmpPd6KUP7FjN3NaFLS0++hub2bqBDMqAYR+ekNYIYxJUBxdW0liENmbEdDapK+/lt6baFErNhp6roz06ccpOBxhq6EtIXm1j67XIWaAALTJyVNC1BaVXsaxEtm7W/mYnOPXa5Ci+TfRN7OE2bNLZ3zUQLih0iarPgYpvrkVTvchJorCNdOKw4sCVBcU9MlpfhzKz6GOXaqhc4erx2uQoVE8JDI/1G3FSeWt5rXX2mpz85IyxSwzIofVZM4HILc+alWs2SZE2evsfuVY7zw2gl+e6Cu/6NzbXUpiTH1KUkxKQxfpSx5TuTv3G01li2H7QoKslzuPvfvgHwrfiIjHDz35N3MTI2zI1umeLuykWd/eRRtlCmTxISojh3fWHYqb1GadMYp68TCnZa7braddizKWzhLKI4qJJYe4QW3J/OPj/8RDpNLkVZobR/gkacO4Rscd1XvshDal/dVnTI86BoN2w7blh493YiqfZ2hq+RNc+bjNnb9qnrMSbtg8svXT05Q+LJHU9TNdhU+2HzcqO5Ka0t2RuoHAvHHWLh2/kJTD6oGi+dbXxfWS+3ZVl4uPj1ekl4plU1l1Sd1LzfqwfbzXvXNrRfnZaT+DsR2QP9y2E2cOdcGwKLs4ItQ19DBv75Yicc75riyW1PkhrLqE+/bHTtoDW1R3uJlQmG/1XfCjm8uZcOaOXZl6zpNLb18WHuF94828fHFzvGSXgaxpaS69rjtmSDIVw5sX7k4M6DyOrDSrA+HQ+Fnf7OW2Rm6NxyPiapJDr17gfIj52m8omvkfVwIbaudbf7NBPXI6UdNLT0zsqP/K2owagYmxwlSSrr7BvlqXqalvAx4/Pz42Xc58E4D3X0TblmUIPb0xPXcf/D9ekOXbxgl6HPAb711wQt8b0v+4hLgF5i4vv7Mx9av83/p9ZOcbdDl5ypS+V7J0Zpyy0F1ELI7X0qqT5Q4lMEcEM8z9PchujFzB8TNvHdswimrgBTsUgLKHaEqfAhBDbiRvZVn24Hvb1mWs0co4qcSsW4iGyHgwe2f2RNsmAHPmLsyJFCKpj1ZeuyU6VlNs0zq3WBFy5csF1L7EbCNUWrj7IwEvrM9h2ULra/fbtvxxs0bsgJAiaLJfy4+dnLCbZfBYkpczrZt1ZIsLaA9CDw4fVp0xuqlGdfuXJHpyM5KTNezoUtXjD8I8IkQ4j9VJy+W/b7Wlql0K0wJAW5ADL7/t8sjoiI3ImUhQz0nS82klDLQ1uG5+mdP7H9J4NhbXFUT8n9KGo+pJsAIZPXOGKSyFCHzkfJ2BLOBLCCRoSvy3Qw1XV3AINAKXAIaEeIUqjiFolWJ/J3hs7VhwoQJEyZMmDBhwoQJEybMlOD/AQQTrpEaYzSCAAAAAElFTkSuQmCC"
 
 ###############################################################################
 # Support
@@ -224,6 +231,7 @@ class SearchThread(QtCore.QThread):
                 comic_soup = BeautifulSoup(comic_html, 'html.parser')
             except requests.exceptions.ConnectionError:
                 log("[ERROR] Cannot connect to server\n" + traceback.format_exc())
+                self.sig.emit(self.MODE_UPDATE_UI, (self.UI_LABEL_ERROR, "[ERROR] Cannot connect to server"))
                 return ERR_WHILE_REQUITING_DATA
             
             try:
@@ -637,8 +645,10 @@ class UiComicDownloaderWindow(QtWidgets.QMainWindow):
         sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
         self.setSizePolicy(sizePolicy)
         self.setWindowTitle("Comic Downloader")
+        pixmap = QtGui.QPixmap()
+        pixmap.loadFromData(QtCore.QByteArray.fromBase64(IMG_LOGO_48))
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("Images/icons8-pluto-dwarf-planet-48.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(pixmap, QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setWindowIcon(icon)
         self.centralWidget = QtWidgets.QWidget(self)
         self.centralWidget.setObjectName("centralWidget")
@@ -685,8 +695,10 @@ class UiComicDownloaderWindow(QtWidgets.QMainWindow):
         sizePolicy.setHeightForWidth(self.btn_push_search.sizePolicy().hasHeightForWidth())
         self.btn_push_search.setSizePolicy(sizePolicy)
         self.btn_push_search.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        pixmap1 = QtGui.QPixmap()
+        pixmap1.loadFromData(QtCore.QByteArray.fromBase64(IMG_SEARCH))
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("Images/icons8-search-24.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon1.addPixmap(pixmap1, QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.btn_push_search.setIcon(icon1)
         self.btn_push_search.setIconSize(QtCore.QSize(24, 24))
         self.btn_push_search.setFlat(False)
@@ -790,6 +802,11 @@ class UiComicDownloaderWindow(QtWidgets.QMainWindow):
             else:
                 print("yo wut?")
         elif data1 == self.search_thread.MODE_SEARCH_RESULT:
+            # clear list
+            for i in reversed(range(self.v_box_layout_results.count())):
+                self.v_box_layout_results.itemAt(i).widget().setParent(None)
+            
+            # repopulate list
             for search_result in data2:
                 self.v_box_layout_results.addWidget(ComicGroupBox(search_result))
 
@@ -836,27 +853,68 @@ class ComicGroupBox(QtWidgets.QGroupBox):
     """
     def __init__(self, comic_data):
         super(ComicGroupBox, self).__init__()
-        self.setFixedSize(760, 160)
+        self.EPISODES = comic_data[0]
+        self.COMIC = comic_data[2]
         
+        self.setFixedSize(760, 170)
+        self.setStyleSheet("border:0 ; background-color:lightgray")
         self.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
         layout_groupbox = QtWidgets.QVBoxLayout(self)
         self.setLayout(layout_groupbox)
         
         # Thumbnail
         thumbnail = QtWidgets.QLabel(self)
+        thumbnail.setScaledContents(True)
         thumbnail.setFixedSize(200, 150)
+        thumbnail.setAlignment(QtCore.Qt.AlignVCenter)
         thumbnail.move(10, 10)
+        
         response = requests.get(comic_data[7], stream=True)
         if response.status_code == 200:
             image = QtGui.QImage()
             image.loadFromData(response.content)
             
-            # thumbnail.setAlignment(QtCore.Qt.AlignCenter)
             thumbnail.setPixmap(QtGui.QPixmap.fromImage(image))
         
         # Title
         title = QtWidgets.QLabel(comic_data[1], self)
-        title.move(300, 0)
+        title.setFixedSize(410, 30)
+        title.setStyleSheet("QLabel { font: 20pt}")
+        title.move(220, 10)
+        
+        # Authors
+        authors = QtWidgets.QLabel(comic_data[4], self)
+        authors.setFixedSize(200, 30)
+        authors.setStyleSheet("QLabel { font: 15pt}")
+        authors.move(220, 41)
+        
+        # Last date of update
+        update = QtWidgets.QLabel("Latest: " + comic_data[5], self)
+        update.setAlignment(QtCore.Qt.AlignRight)
+        update.setFixedSize(200, 30)
+        update.setStyleSheet("QLabel { font: 15pt}")
+        update.move(550, 41)
+        
+        # Number of episodes
+        number_of_episodes = QtWidgets.QLabel("%s episodes" % comic_data[6], self)
+        number_of_episodes.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+        number_of_episodes.setFixedSize(110, 30)
+        number_of_episodes.setStyleSheet("QLabel { font: 12pt}")
+        number_of_episodes.move(640, 10)
+        
+        # Synopsis
+        synopsis = QtWidgets.QTextEdit(comic_data[3], self)
+        synopsis.setReadOnly(True)
+        synopsis.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+        synopsis.setFixedSize(450, 75)
+        synopsis.move(220, 90)
+        
+        # Download
+        btn_download = QtWidgets.QPushButton(self)
+        btn_download_pixmap = QtGui.QPixmap()
+        btn_download_pixmap.loadFromData(QtCore.QByteArray.fromBase64(IMG_DOWNLOAD))
+        btn_download.setIconSize(QtCore.QSize(32, 32))
+        btn_download.setIcon(QtGui.QIcon(btn_download_pixmap))
         
         
 if __name__ == "__main__":
